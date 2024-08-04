98b23c20acf17de8a3e879ddfea6f9d8f65fb580
caimez323 <caimezae@gmail.com>
2024-08-04 03:27:29 +0200

    add 3 top results for yt search

--- fonctions/youtube_search.py
+++ fonctions/youtube_search.py
@@ -11,5 +11,17 @@ async def youtube_search(message):
 
   query_string = urllib.parse.urlencode({'search_query': search})
   htm_content = urllib.request.urlopen('http://www.youtube.com/results?' + query_string)
+
   search_results = re.findall(r'/watch\?v=(.{11})', htm_content.read().decode())
-  await message.channel.send('http://www.youtube.com/watch?v=' + search_results[0])
+  displayString = ""
+  checkList = []
+  index = 0
+  while len(checkList) <3 and index <9999:
+    print(index)
+    theString = 'http://www.youtube.com/watch?v={}'.format(search_results[index])
+    index+=1
+    if theString in checkList:
+      continue
+    checkList.append(theString)
+    displayString+=(theString+"\n")
+  await message.channel.send(displayString)
